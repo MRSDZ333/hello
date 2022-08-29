2eb6d961909faa4ef90f8148a527cc34a8a7fce4
priyankka-n <npriyankka0@gmial.com>
2022-08-29 21:49:25 +0000

    added name and major to code

--- hello.c
+++ hello.c
@@ -1,9 +1,13 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(){
 
     printf("sandeshsubedi !\n");
     printf("computer science\n");
+
+    printf("Priyankka Nanrudaiyan\n");
+    printf("Electrical Engineering\n");
 
 return 0;
 
